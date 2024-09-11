ae00f1de118ce67a043e974931c0e0af1ccfb568
Augusto Napoleão <gutinho.amorim@gmail.com>
2024-09-10 21:21:38 -0300

    Fundamentos 002

---
+++ index.js
@@ -0,0 +1,13 @@
+// array, objetos
+let meta = {
+    value: 'ler um livro no mês',
+    checked: false,
+    log: (info) => {
+        console.log(info)
+    }
+}
+meta.value = "Não é mais ler um livro"
+meta.log(meta.value)
+// function // arrow function
+
+const criarMeta = () => {}
