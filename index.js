c8e17aea95baa572d0c802c5efcc894b72879f0a
Augusto Napoleão <gutinho.amorim@gmail.com>
2024-09-12 09:50:48 -0300

    Fundamentos 005

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const start = () => {
     let count = 1
-    while(count <= 10){
-        console.log(count)
-        count = count + 1
+    while(true){
+        let opcao = "cadastrar"
+        switch (opcao) {
+            case "cadastrar":
+                console.log("vamos cadastrar")
+                break
+            case "listas":
+                console.log("listar")
+                break
+            case "sair":
+                return
+
+        }
+        
     }
 }
 
