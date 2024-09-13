ac5c031654f44222ae113df9fe05eafa6de53b49
Augusto Napoleão <gutinho.amorim@gmail.com>
2024-09-13 11:43:34 -0300

    Deletar metas

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ const metasRealizadas = async () => {
     }
     
     await select({
-        message: "Metas realizadas " + realizadas.length,
+        message: "Metas realizadas: " + realizadas.length,
         choices: [...realizadas]
     })
 }
@@ -71,12 +71,35 @@ const metasAbertas = async () => {
     }
     
     await select({
-        message: "Metas abertas " + abertas.length,
+        message: "Metas abertas: " + abertas.length,
         choices: [...abertas]
     })
 }
 
+const deletarMetas = async () => {
+    const metasDesmarcadas = metas.map((meta) => {
+        return { value: meta.value, checked: false }
+    })
+    
+    const itensADeletar = await checkbox({
+        message: "Selecione quais metas deseja deletar!",
+        choices: [...metasDesmarcadas],
+        instructions: false,
+    })
 
+    if(itensADeletar.length ==0) {
+        console.log("Nenhum item para deletar!")
+        return
+    }
+    
+    itensADeletar.forEach((item) => {
+        metas = metas.filter((meta) => {
+            return meta.value != item 
+        })
+    })
+    
+    console.log("Metas deletadas com sucesso!!!")
+}
 const start = async () => {
     while(true){
         
@@ -100,6 +123,10 @@ const start = async () => {
                     value: "abertas"
                 },
                 {
+                    name: "Deletar metas",
+                    value: "deletar"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -118,6 +145,9 @@ const start = async () => {
                 break
             case "abertas":
                 await metasAbertas()
+                break
+            case "deletar":
+                await deletarMetas()
                 break
             case "sair":
                 console.log("Até a próxima")
